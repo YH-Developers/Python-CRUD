0c0997b8cf3f6679e1b0e550f679f0c221338381
Muhammad Yasir Hassan <99041884+Code-With-Yasir@users.noreply.github.com>
2022-10-08 11:41:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtGui import QPixmap
 
 my_app=QtWidgets.QApplication([])
-my_ui =uic.loadUi("User_interface.ui")
+my_ui=uic.loadUi("Main.ui")
 
 
 def convert():
@@ -17,6 +17,8 @@ def convert():
 
 
 # code for add district
+
+my_ui.District.setPlaceholderText("Select District")
 my_ui.District.addItem('Lahore')
 my_ui.District.addItem('Attock')
 my_ui.District.addItem('Mianwali')
@@ -49,10 +51,30 @@ my_ui.District.addItem('Toba Tekh singh')
 my_ui.District.addItem('Vehari')
 
 # code to add Tehsil
+my_ui.comboBox.addItem('Lahore')
+#code to add stamp type
+my_ui.comboBox_2.setPlaceholderText("Select Stamp paper type")
+my_ui.comboBox_2.addItem("Judicial") 
+my_ui.comboBox_2.addItem("Non Judicial") 
+
+# code for add Deed
+my_ui.comboBox_3.setPlaceholderText("Select Deed")
+my_ui.comboBox_3.addItem("Court Fee") 
+
+
+# NAme
+my_ui.AgentName.setPlaceholderText("Enter Your name")
+
+my_ui.AgentCNIC.setPlaceholderText("Enter Your CNIC")
+
+
+my_ui.AgentContact.setPlaceholderText("+923XXXXXXXXX")
+
+
+my_ui.AgentEmail.setPlaceholderText("XXXXXXX@gmail.com")
 
 
 
-# code for add Tehsil
 
 
 
